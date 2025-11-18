eefc3529de38375265131481591b85fc67d7a8fc
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-11-18 13:03:35 +0530

    Fix Commit.

--- Relevantz.EEPZ.InternalOpportunityAPI/Relevantz.EEPZ.Core/obj/Debug/net8.0/Relevantz.EEPZ.Core.AssemblyInfo.cs
+++ Relevantz.EEPZ.InternalOpportunityAPI/Relevantz.EEPZ.Core/obj/Debug/net8.0/Relevantz.EEPZ.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Relevantz.EEPZ.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f893d85af9326d5f2ec67406b1974a12388bf22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b441b90e55d12b567bbc0d67e22a36f9a0ac1840")]
 [assembly: System.Reflection.AssemblyProductAttribute("Relevantz.EEPZ.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Relevantz.EEPZ.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Relevantz.EEPZ.LnDApi/Relevantz.EEPZ.Api/obj/Debug/net8.0/Relevantz.EEPZ.Api.AssemblyInfo.cs
+++ Relevantz.EEPZ.LnDApi/Relevantz.EEPZ.Api/obj/Debug/net8.0/Relevantz.EEPZ.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Relevantz.EEPZ.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3583c80b9330847356d2d96eef8be4d0b4b872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b441b90e55d12b567bbc0d67e22a36f9a0ac1840")]
 [assembly: System.Reflection.AssemblyProductAttribute("Relevantz.EEPZ.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Relevantz.EEPZ.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
