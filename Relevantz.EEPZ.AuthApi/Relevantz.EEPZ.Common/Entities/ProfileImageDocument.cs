cf8e7149b1b4353532278a803bf969fe2f740e77
Rishikesvavigneshwar <rushivigneshwar@gmail.com>
2026-01-07 18:00:56 +0530

    Fix : Refactroing the code quality of  Entities In Auth Api

--- Relevantz.EEPZ.AuthApi/Relevantz.EEPZ.Common/Entities/ProfileImageDocument.cs
+++ Relevantz.EEPZ.AuthApi/Relevantz.EEPZ.Common/Entities/ProfileImageDocument.cs
@@ -1,6 +1,5 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
 namespace Relevantz.EEPZ.Common.Entities
 {
     public class ProfileImageDocument
@@ -8,25 +7,18 @@ namespace Relevantz.EEPZ.Common.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        
         [BsonElement("employee_id")]
         public int EmployeeId { get; set; }
-        
         [BsonElement("image_data")]
         public byte[] ImageData { get; set; }
-        
         [BsonElement("content_type")]
         public string ContentType { get; set; }
-        
         [BsonElement("file_name")]
         public string FileName { get; set; }
-        
         [BsonElement("file_size")]
         public long FileSize { get; set; }
-        
         [BsonElement("uploaded_at")]
         public DateTime UploadedAt { get; set; }
-        
         [BsonElement("updated_at")]
         public DateTime? UpdatedAt { get; set; }
     }
