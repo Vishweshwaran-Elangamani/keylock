a66a23bc0d0426bff0ee40d8b8193ffd5f5ef714
rajeshpopz68 <rajeshpopz68@gmail.com>
2026-01-21 17:13:36 +0530

    Added unit testing

---
+++ Relevantz.EEPZ.RewardsApi/Relevantz.EEPZ.Api.Tests/Helpers/TestResponse.cs
@@ -0,0 +1,9 @@
+namespace Relevantz.EEPZ.Api.Tests.Helpers
+{
+    public class TestResponse
+    {
+        public bool success { get; set; }
+        public object? data { get; set; }
+        public string? message { get; set; }
+    }
+}
