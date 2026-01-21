4192547f92e9562f28c9651838770ac349251b4e
rajeshpopz68 <rajeshpopz68@gmail.com>
2026-01-21 17:46:52 +0530

    Added Unit test for Rewards API

--- Relevantz.EEPZ.RewardsApi/Relevantz.EEPZ.Api.Tests/UnitTest1.cs
+++ Relevantz.EEPZ.RewardsApi/Relevantz.EEPZ.Api.Tests/UnitTest1.cs
@@ -1,7 +1,5 @@
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authorization;
@@ -10,33 +8,11 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using PerformanceManagement.Controllers;
-using Relevantz.EEPZ.API.Controllers;
 using Relevantz.EEPZ.Api.Tests.Helpers;
 using Relevantz.EEPZ.Business.Services.Interfaces;
-using Relevantz.EEPZ.Common.DTOs.Response;
-using Relevantz.EEPZ.Core.Services.Interfaces;
 
 namespace Relevantz.EEPZ.Api.Tests.Controllers
 {
-    internal static class AttributeAssertions
-    {
-        public static void ShouldHaveAttribute<TAttribute>(
-            this MemberInfo member,
-            Func<TAttribute, bool>? predicate = null
-        )
-            where TAttribute : Attribute
-        {
-            var attr = member.GetCustomAttribute<TAttribute>();
-            attr.Should()
-                .NotBeNull($"Expected attribute {typeof(TAttribute).Name} on {member.Name}");
-
-            if (predicate != null)
-                predicate(attr!)
-                    .Should()
-                    .BeTrue($"Attribute {typeof(TAttribute).Name} did not satisfy conditions.");
-        }
-    }
-
     [TestFixture]
     public class DepartmentHeadNominationControllerTests
     {
@@ -49,21 +25,20 @@ namespace Relevantz.EEPZ.Api.Tests.Controllers
         {
             _serviceMock = new Mock<IDepartmentHeadNominationService>(MockBehavior.Strict);
             _loggerMock = new Mock<ILogger<DepartmentHeadNominationController>>();
-            _controller = new DepartmentHeadNominationController(
-                _serviceMock.Object,
-                _loggerMock.Object
-            );
+            _controller = new DepartmentHeadNominationController(_serviceMock.Object, _loggerMock.Object);
         }
 
+        // ========================================================
+        // APPROVED NOMINATIONS
+        // ========================================================
         [Test]
         public async Task GetApprovedNominationsByDeptHead_ReturnsOk_WhenSuccessTrue()
         {
-            var id = 100;
+            int id = 100;
             var serviceResult = new TestResponse { success = true, data = new[] { 1, 2 } };
 
-            _serviceMock
-                .Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id))
-                .ReturnsAsync(serviceResult);
+            _serviceMock.Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id))
+                        .ReturnsAsync(serviceResult);
 
             var result = await _controller.GetApprovedNominationsByDeptHead(id);
 
@@ -75,12 +50,11 @@ namespace Relevantz.EEPZ.Api.Tests.Controllers
         [Test]
         public async Task GetApprovedNominationsByDeptHead_ReturnsNotFound_WhenSuccessFalse()
         {
-            var id = 100;
+            int id = 100;
             var serviceResult = new TestResponse { success = false, message = "Not found" };
 
-            _serviceMock
-                .Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id))
-                .ReturnsAsync(serviceResult);
+            _serviceMock.Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id))
+                        .ReturnsAsync(serviceResult);
 
             var result = await _controller.GetApprovedNominationsByDeptHead(id);
 
@@ -92,219 +66,131 @@ namespace Relevantz.EEPZ.Api.Tests.Controllers
         [Test]
         public async Task GetApprovedNominationsByDeptHead_Returns500_OnException()
         {
-            var id = 100;
+            int id = 100;
             var ex = new Exception("Boom");
 
-            _serviceMock.Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id)).ThrowsAsync(ex);
+            _serviceMock.Setup(s => s.GetApprovedNominationsByDeptHeadAsync(id))
+                        .ThrowsAsync(ex);
 
             var result = await _controller.GetApprovedNominationsByDeptHead(id);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(500);
-            obj.Value.Should()
-                .BeEquivalentTo(new { success = false, message = $"Error: {ex.Message}" });
+            obj.Value.Should().BeEquivalentTo(new
+            {
+                success = false,
+                message = $"Error: {ex.Message}"
+            });
         }
 
+        // ========================================================
+        // NOMINATION DETAILS
+        // ========================================================
         [Test]
         public async Task GetNominationDetails_ReturnsOk_WhenSuccessTrue()
         {
-            var resultDto = new TestResponse { success = true, data = new { Id = 10 } };
+            int nominationId = 10;
+            var serviceResult = new TestResponse { success = true, data = new { Id = 10 } };
 
-            _serviceMock.Setup(s => s.GetNominationDetailsAsync(10)).ReturnsAsync(resultDto);
+            _serviceMock.Setup(s => s.GetNominationDetailsAsync(nominationId))
+                        .ReturnsAsync(serviceResult);
 
-            var result = await _controller.GetNominationDetails(10);
+            var result = await _controller.GetNominationDetails(nominationId);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(200);
-            obj.Value.Should().BeEquivalentTo(resultDto);
+            obj.Value.Should().BeEquivalentTo(serviceResult);
         }
 
         [Test]
         public async Task GetNominationDetails_ReturnsNotFound_WhenSuccessFalse()
         {
-            var resultDto = new TestResponse { success = false, message = "Not found" };
+            int nominationId = 10;
+            var serviceResult = new TestResponse { success = false, message = "Not found" };
 
-            _serviceMock.Setup(s => s.GetNominationDetailsAsync(10)).ReturnsAsync(resultDto);
+            _serviceMock.Setup(s => s.GetNominationDetailsAsync(nominationId))
+                        .ReturnsAsync(serviceResult);
 
-            var result = await _controller.GetNominationDetails(10);
+            var result = await _controller.GetNominationDetails(nominationId);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(404);
-            obj.Value.Should().BeEquivalentTo(resultDto);
+            obj.Value.Should().BeEquivalentTo(serviceResult);
         }
 
         [Test]
         public async Task GetNominationDetails_Returns500_OnException()
         {
+            int nominationId = 10;
             var ex = new Exception("Unexpected!");
 
-            _serviceMock.Setup(s => s.GetNominationDetailsAsync(10)).ThrowsAsync(ex);
+            _serviceMock.Setup(s => s.GetNominationDetailsAsync(nominationId))
+                        .ThrowsAsync(ex);
 
-            var result = await _controller.GetNominationDetails(10);
+            var result = await _controller.GetNominationDetails(nominationId);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(500);
-            obj.Value.Should().BeEquivalentTo(new { success = false, message = ex.Message });
+            obj.Value.Should().BeEquivalentTo(new
+            {
+                success = false,
+                message = ex.Message
+            });
         }
 
+        // ========================================================
+        // DEPARTMENT STATISTICS
+        // ========================================================
         [Test]
         public async Task GetDepartmentStatistics_ReturnsOk_WhenSuccessTrue()
         {
-            var resultDto = new TestResponse { success = true, data = new { total = 5 } };
+            int id = 50;
+            var serviceResult = new TestResponse { success = true, data = new { total = 5 } };
 
-            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(50)).ReturnsAsync(resultDto);
+            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(id))
+                        .ReturnsAsync(serviceResult);
 
-            var result = await _controller.GetDepartmentStatistics(50);
+            var result = await _controller.GetDepartmentStatistics(id);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(200);
-            obj.Value.Should().BeEquivalentTo(resultDto);
+            obj.Value.Should().BeEquivalentTo(serviceResult);
         }
 
         [Test]
         public async Task GetDepartmentStatistics_ReturnsNotFound_WhenSuccessFalse()
         {
-            var resultDto = new TestResponse { success = false, message = "Not found" };
+            int id = 50;
+            var serviceResult = new TestResponse { success = false, message = "Not found" };
 
-            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(50)).ReturnsAsync(resultDto);
+            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(id))
+                        .ReturnsAsync(serviceResult);
 
-            var result = await _controller.GetDepartmentStatistics(50);
+            var result = await _controller.GetDepartmentStatistics(id);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(404);
-            obj.Value.Should().BeEquivalentTo(resultDto);
+            obj.Value.Should().BeEquivalentTo(serviceResult);
         }
 
         [Test]
         public async Task GetDepartmentStatistics_Returns500_OnException()
         {
+            int id = 50;
             var ex = new Exception("Oops");
 
-            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(50)).ThrowsAsync(ex);
+            _serviceMock.Setup(s => s.GetDepartmentStatisticsAsync(id))
+                        .ThrowsAsync(ex);
 
-            var result = await _controller.GetDepartmentStatistics(50);
-
-            var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
-            obj.StatusCode.Should().Be(500);
-            obj.Value.Should().BeEquivalentTo(new { success = false, message = ex.Message });
-        }
-    }
-
-    //  EmployeeNominationController Tests
-    [TestFixture]
-    public class EmployeeNominationControllerTests
-    {
-        private Mock<IEmployeeNominationService> _serviceMock = null!;
-        private Mock<ILogger<EmployeeNominationController>> _loggerMock = null!;
-        private EmployeeNominationController _controller = null!;
-
-        [SetUp]
-        public void Setup()
-        {
-            _serviceMock = new Mock<IEmployeeNominationService>(MockBehavior.Strict);
-            _loggerMock = new Mock<ILogger<EmployeeNominationController>>();
-            _controller = new EmployeeNominationController(_serviceMock.Object, _loggerMock.Object);
-        }
-
-        [Test]
-        public async Task SearchEmployeeNotifications_ReturnsOk_WhenSuccessTrue()
-        {
-            var employeeId = 42;
-
-            var items = new List<EmployeeNotificationItemDto>
-            {
-                new EmployeeNotificationItemDto { NominationId = 1, RoleType = "Gold" },
-                new EmployeeNotificationItemDto { NominationId = 2, RoleType = "Silver" },
-            };
-
-            var serviceResult = new EmployeeNotificationSearchResultDto
-            {
-                Success = true,
-                Message = "Found",
-                Data = items,
-            };
-
-            _serviceMock
-                .Setup(s => s.SearchEmployeeNotificationsAsync(employeeId))
-                .ReturnsAsync(serviceResult);
-
-            var result = await _controller.SearchEmployeeNotifications(employeeId);
-
-            var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
-            obj.StatusCode.Should().Be(200);
-            obj.Value.Should()
-                .BeEquivalentTo(
-                    new
-                    {
-                        success = true,
-                        data = items,
-                        count = items.Count,
-                        message = "Found",
-                    }
-                );
-        }
-
-        [Test]
-        public async Task SearchEmployeeNotifications_ReturnsBadRequest_WhenInvalidEmployeeIdMessage()
-        {
-            var employeeId = -1;
-
-            var serviceResult = new EmployeeNotificationSearchResultDto
-            {
-                Success = false,
-                Message = "Please enter a valid employee ID",
-            };
-
-            _serviceMock
-                .Setup(s => s.SearchEmployeeNotificationsAsync(employeeId))
-                .ReturnsAsync(serviceResult);
-
-            var result = await _controller.SearchEmployeeNotifications(employeeId);
-
-            var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
-            obj.StatusCode.Should().Be(400);
-            obj.Value.Should()
-                .BeEquivalentTo(
-                    new { success = false, message = "Please enter a valid employee ID" }
-                );
-        }
-
-        [Test]
-        public async Task SearchEmployeeNotifications_Returns500_WhenServiceFailure()
-        {
-            var employeeId = 77;
-
-            var serviceResult = new EmployeeNotificationSearchResultDto
-            {
-                Success = false,
-                Message = "Something failed",
-            };
-
-            _serviceMock
-                .Setup(s => s.SearchEmployeeNotificationsAsync(employeeId))
-                .ReturnsAsync(serviceResult);
-
-            var result = await _controller.SearchEmployeeNotifications(employeeId);
+            var result = await _controller.GetDepartmentStatistics(id);
 
             var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
             obj.StatusCode.Should().Be(500);
-            obj.Value.Should()
-                .BeEquivalentTo(new { success = false, message = "Something failed" });
-        }
-
-        [Test]
-        public async Task SearchEmployeeNotifications_Returns500_OnException()
-        {
-            var employeeId = 100;
-            var ex = new Exception("Unexpected!");
-
-            _serviceMock.Setup(s => s.SearchEmployeeNotificationsAsync(employeeId)).ThrowsAsync(ex);
-
-            var result = await _controller.SearchEmployeeNotifications(employeeId);
-
-            var obj = result.Should().BeAssignableTo<ObjectResult>().Subject;
-            obj.StatusCode.Should().Be(500);
-            obj.Value.Should().BeEquivalentTo(new { success = false, message = ex.Message });
+            obj.Value.Should().BeEquivalentTo(new
+            {
+                success = false,
+                message = ex.Message
+            });
         }
     }
 }
